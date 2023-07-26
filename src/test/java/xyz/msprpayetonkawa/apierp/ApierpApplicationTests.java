a8548e42d87dcf7da26c6f5f93d7680af1bb65c7
Floutts <maxence.maziere@epsi.fr>
2023-07-26 15:58:35 +0200

    Test ProductService réalisés

--- src/test/java/xyz/msprpayetonkawa/apierp/ApierpApplicationTests.java
+++ src/test/java/xyz/msprpayetonkawa/apierp/ApierpApplicationTests.java
@@ -1,13 +1,15 @@
 package xyz.msprpayetonkawa.apierp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import xyz.msprpayetonkawa.apierp.service.ProductServiceTest;
 
-@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		ProductServiceTest.class
+		// Add more test classes here if needed
+})
 class ApierpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
---
+++ src/test/java/xyz/msprpayetonkawa/apierp/service/ProductServiceTest.java
@@ -0,0 +1,62 @@
+package xyz.msprpayetonkawa.apierp.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.msprpayetonkawa.apierp.product.Product;
+import xyz.msprpayetonkawa.apierp.product.ProductRepository;
+import xyz.msprpayetonkawa.apierp.product.ProductService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class ProductServiceTest {
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Before
+    public void setUp(){
+        Product product1 = new Product(1L,"uid-key","Name","Description",11.11f,1);
+        Product product2 = new Product(2L,"other-uid-key","Other-Name","Other-Description",22.22f,2);
+        List<Product> products = Arrays.asList(product1, product2);
+
+        Mockito.when(productRepository.findAll()).thenReturn(products);
+        Mockito.when(productRepository.findByUid("uid-key")).thenReturn(product1);
+        Mockito.when(productRepository.findByUid("no-uid-key")).thenReturn(null);
+
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Product> result = productService.getProducts();
+        assertEquals(2,result.size());
+    }
+
+    @Test
+    public void testFindByUidThatExists(){
+        String uid = "1uid-key";
+        Product result = productService.getProduct(uid);
+        assertEquals(1L,result.getId());
+    }
+
+    @Test
+    public void testFindByUidThatDoesNotExists(){
+        String uid = "no-uid-key";
+        Product result = productService.getProduct(uid);
+        assertNull(result);
+    }
+}
