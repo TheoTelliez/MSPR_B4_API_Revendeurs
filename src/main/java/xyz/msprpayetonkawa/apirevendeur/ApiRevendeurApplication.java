1167c6dd7c59f9e800309a057236a554613c2701
kipsta311001 <fabienbayon@yahoo.fr>
2023-08-26 09:16:55 +0200

    modif on swagger

--- src/main/java/xyz/msprpayetonkawa/apirevendeur/ApiRevendeurApplication.java
+++ src/main/java/xyz/msprpayetonkawa/apirevendeur/ApiRevendeurApplication.java
@@ -1,24 +1,26 @@
 package xyz.msprpayetonkawa.apirevendeur;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 
 @SpringBootApplication
+
 public class ApiRevendeurApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRevendeurApplication.class, args);
 	}
-	
+
 	@Bean
     public OpenAPI apiDocConfig() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API CRM")
-                        .description("API for CRM")
+                        .title("API REVENDEURS")
+                        .description("API for Revendeurs")
                         .version("0.0.1"))
                 .externalDocs(new ExternalDocumentation()
                         .description("Documentation")
